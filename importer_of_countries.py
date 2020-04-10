4999a50926cb22d4bc586d3be22bcb969184dbe7
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-04-10 21:11:16 +0200

    refactor def load data from api

--- importer_of_countries.py
+++ importer_of_countries.py
@@ -11,12 +11,17 @@ def load_data_from_api(url):
         solution = []
         for item in data_load:
             name = (item['name'])
-            iso = (item['alpha3Code'])
+            symbol_of_country = (item['alpha3Code'])
             country_code = (item['numericCode'])
             population = (item['population'])
             latlag = (item['latlng'])
             flag = (item['flag'])
-            row = [name, iso, country_code, population, latlag, flag]
+            row = {'name': name,
+                   'symbol_of_country': symbol_of_country,
+                   'country_code': country_code,
+                   'population': population,
+                   'latlag': latlag,
+                   'flag': flag}
             solution.append(row)
         return solution
 
