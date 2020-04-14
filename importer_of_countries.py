49eadb4b1871bcbda6b3e7ee3ebfab61403c96ff
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-04-15 01:30:07 +0200

    Big refactor, change most  to classes, protect sql query to sql injection, added importer json from url api - current data

--- importer_of_countries.py
+++ importer_of_countries.py
@@ -1,51 +1,50 @@
 import json
 import sqlite3
 from urllib.request import urlopen
-from conect_to_db import connect_to_db
-
-API = "https://restcountries.eu/rest/v2/all"
-
-
-def load_data_from_api(url):
-    with urlopen(url) as file:
-        response = file.read()
-        data_load = json.loads(response)
-        print(data_load)
-        solution = []
-        for item in data_load:
-            name = (item['name'])
-            alpha3code = (item['alpha3Code'])
-            alpha2code = (item['alpha2Code'])
-            population = (item['population'])
-            latlag = (item['latlng'])
-            flag = (item['flag'])
-            row = {'name': name,
-                   'alpha3code': alpha3code,
-                   'alpha2code': alpha2code,
-                   'population': population,
-                   'latlag': str(latlag),
-                   'flag': str(flag)}
-            solution.append(row)
-        return solution
+from conect_to_db import ConnectToDb
+from path_and_api import *
 
 
-def insert_data_to_db(data):
-    conn = connect_to_db()
-    c = conn.cursor()
-    for row in data:
-        row = (row.values())
-        query = 'INSERT INTO countries VALUES(null, "{}", "{}", "{}", {}, "{}", "{}");'.format(*row)
-        print(query)
-        try:
-            c.execute(query)
-            conn.commit()
-        except sqlite3.IntegrityError:
-            continue
-    conn.close()
-    return print(f'--> Script executed pass <--')
+class ImporterOfCountries(ConnectToDb):
+
+    def load_data_from_api(self, url):
+        with urlopen(url) as file:
+            response = file.read()
+            data_load = json.loads(response)
+            solution = []
+            for item in data_load:
+                name = (item['name'])
+                alpha3code = (item['alpha3Code'])
+                alpha2code = (item['alpha2Code'])
+                population = (item['population'])
+                latlag = (item['latlng'])
+                flag = (item['flag'])
+                row = {'name': name,
+                       'alpha3code': alpha3code,
+                       'alpha2code': alpha2code,
+                       'population': population,
+                       'latlag': str(latlag),
+                       'flag': str(flag)}
+                solution.append(row)
+            print(f'--> Script {ImporterOfCountries.load_data_from_api.__name__} executed <--')
+            return solution
+
+    def insert_data_to_db(self, data):
+        conn = ConnectToDb()
+        for row in data:
+            parameters = row.values()
+            parameters = tuple(parameters)
+            query = 'INSERT INTO countries VALUES(null, ?, ?, ?, ?, ?, ?);'
+            try:
+                conn.insert_record(query=query, parameters=parameters)
+                print('Insert record: ', parameters)
+            except sqlite3.IntegrityError:
+                continue
+        conn.close_connect()
+        return print(f'--> Script {ImporterOfCountries.insert_data_to_db.__name__} executed <--')
 
 
 if __name__ == '__main__':
-    data_slice = load_data_from_api(API)
-
-    insert_data_to_db(data_slice)
+    importer = ImporterOfCountries()
+    data = importer.load_data_from_api(url=JsonApi.API_COUNTRIES)
+    importer.insert_data_to_db(data=data)
---
+++ path_and_api.py
@@ -0,0 +1,10 @@
+class Files:
+    JSON_ALL_DATA = "./resources/json/all_data.json"
+    JSON_CURRENT_DATA = "./resources/json/current_data.json"
+    SCRIPT = './resources/sql/db_init.sql'
+
+
+class JsonApi:
+    API_COUNTRIES = "https://restcountries.eu/rest/v2/all"
+    API_CURRENT_CASES = "https://api.covid19api.com/summary"
+    API_HISTORICAL_CASES = "https://api.covid19api.com/all"
