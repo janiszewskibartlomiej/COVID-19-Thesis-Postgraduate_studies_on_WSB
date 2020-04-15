ac99f44ca260f514745995ae7289697e126ffc4b
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-04-15 21:58:55 +0200

    added new file

--- importer_all_cases_from _json.py
+++ importer_all_cases_from _json.py
@@ -1,35 +1,13 @@
-import json
 import time
-import requests
-
-from connect_to_db import ConnectToDb
 from path_and_api import *
 
 
-class ImporterCurrentCases(ConnectToDb):
+class ImporterAllCases():
 
-    def read_json_file(self, path):
 
-        with open(path, mode='r', encoding='utf-8') as f:
-            data = json.load(f)
-        return data
+    def load_all_data_from_json_and_insert_to_db(self, path, api=True):
 
-    def read_json_api(self, api):
-
-        response = requests.get(api)
-        data = response.json()
-        return data
-
-    def load_data_and_write_json(self, url, file_name):
-        response = requests.get(url)
-        data = response.json()
-        print(f'--> Success write of "{file_name}"" <--')
-        with open(file=file_name, mode='w', encoding='utf-8') as f:
-            json.dump(data, f)
-
-    def load_current_data_from_json_and_insert_to_db(self, path, api=True):
-
-        importer = ImporterCurrentCases()
+        importer = ImporterAllCases()
         if api is True:
             data = importer.read_json_api(path)
         else:
@@ -73,28 +51,14 @@ class ImporterCurrentCases(ConnectToDb):
         print(f'--> Insert cases from json {path} is done <--')
         importer.close_connect()
 
-    def load_name_and_id_of_countries(self):
-
-        query = 'SELECT alpha_2_code, id FROM countries;'
-        connect = ConnectToDb()
-        list_of_data = connect.select_all_record(query=query, parameter='')
-        connect.close_connect()
-        print(f'--> Script {ImporterCurrentCases.load_name_and_id_of_countries.__name__} executed <--')
-        return list_of_data
-
-    def create_dict_of_countries_name_and_id(self, data):
-
-        countries_and_id_dict = {}
-        for row in data:
-            countries_and_id_dict[row[0]] = row[1]
-        print(f'--> Script {ImporterCurrentCases.create_dict_of_countries_name_and_id.__name__} executed <--')
-        return countries_and_id_dict
-
 
 if __name__ == '__main__':
-    importer = ImporterCurrentCases()
-    importer.load_data_and_write_json(JsonApi.API_CURRENT_CASES, Files.JSON_CURRENT_DATA)
-    importer.load_data_and_write_json(JsonApi.API_HISTORICAL_CASES, Files.JSON_ALL_DATA)
+    importer = ImporterAllCases()
+    # importer.load_data_and_write_json(JsonApi.API_CURRENT_CASES, Files.JSON_CURRENT_DATA)
+    # importer.load_data_and_write_json(JsonApi.API_HISTORICAL_CASES, Files.JSON_ALL_DATA)
 
-    importer.load_current_data_from_json_and_insert_to_db(path=JsonApi.API_CURRENT_CASES)
-    importer.load_current_data_from_json_and_insert_to_db(path='./resources/json/current_data.json', api=False)
+    # importer.load_data_from_json_and_insert_to_db(path=JsonApi.API_CURRENT_CASES)
+    # importer.load_data_from_json_and_insert_to_db(path='./resources/json/current_data.json_12', api=False)
+
+    data = importer.read_json_api(JsonApi.API_HISTORICAL_CASES)
+    print('len: ', len(data), '\n', data[0].keys(), '\n', data[0:3], end='\n')
