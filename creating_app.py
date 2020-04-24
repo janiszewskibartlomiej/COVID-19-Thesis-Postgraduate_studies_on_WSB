6f5e063b4404ca63fd3987a8e1eb89e1c140ac6e
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-04-24 13:57:39 +0200

    upload run section

--- creating_app.py
+++ creating_app.py
@@ -1,6 +1,8 @@
 from data_processing import DataProcessing
 from path_and_api import *
 from importer_all_cases_json import ImporterAllCases
+from importer_current_cases_json import ImporterCurrentCases
+from app import app
 
 if __name__ == '__main__':
     run = ImporterAllCases()
@@ -11,5 +13,8 @@ if __name__ == '__main__':
     run.insert_countries_to_db(countries_data)
 
     run.load_all_data_from_json_and_insert_to_db(path=JsonApi.API_ALL_CASES, api=True)
+    ImporterCurrentCases().load_current_data_from_json_and_insert_to_db(path=JsonApi.API_CURRENT_CASES, api=True)
 
     DataProcessing().creating_map()
+
+    app.run(debug=True)
