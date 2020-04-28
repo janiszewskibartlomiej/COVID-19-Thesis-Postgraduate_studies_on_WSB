cc77a94dcd65c0b6976ba3464a379266392f68ef
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-04-28 14:45:18 +0200

    refactor graphs.py - more flex

--- graphs.py
+++ graphs.py
@@ -8,28 +8,33 @@ class Graphs(DataProcessing):
     def __init__(self):
         super().__init__()
 
-    def creating_one_graph(self, dataframe, title_of_graph=None, country_id=None, id=False):
-        if id:
-            name = ConnectToDb().select_one_record(query='SELECT name from countries WHERE country_id = ?',
-                                                   parameter=country_id)
+    def creating_one_graph(self, dataframe, country_id=0):
+        if country_id > 0:
+            name = ConnectToDb().select_one_record(
+                query='SELECT name, alpha_3_code from countries WHERE country_id = ?',
+                parameter=(country_id,))
             title_of_graph = f'Cases of the {name[0]}'
+            alpha_3_code = name[1]
+
+        elif country_id == 0:
+            title_of_graph = 'Cases of the World'
+            alpha_3_code = ''
 
         fig = go.Figure()
 
-
         fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Confirmed'],
                                  mode='lines',
-                                 name='Confirmed',
+                                 name='Confirmed'+alpha_3_code,
                                  line=dict(color='red', width=2, dash='solid')))
 
         fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Deaths'],
                                  mode='lines',
-                                 name='Deaths',
+                                 name='Deaths'+alpha_3_code,
                                  line=dict(color='black', width=2, dash='solid')))
 
         fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Recovered'],
                                  mode='lines',
-                                 name='Recovered',
+                                 name='Recovered'+alpha_3_code,
                                  line=dict(color='springgreen', width=2, dash='solid')))
         fig.update_layout(title=dict(text=title_of_graph, y=0.94,
                                      x=0.47, xanchor='center', yanchor='top', font_size=32,
@@ -46,13 +51,13 @@ class Graphs(DataProcessing):
     def cases_of_the_world(self):
         data = DataProcessing().total_cases_per_day()
         df = DataProcessing().creating_dateframe(data=data)
-        world = Graphs().creating_one_graph(dataframe=df, title_of_graph='Cases of the World')
+        world = Graphs().creating_one_graph(dataframe=df, country_id=0 )
         return world
 
     def cases_of_the_poland(self):
         data = DataProcessing().all_cases_per_day_where_country_id_equal(country_id=(179,))
         df = DataProcessing().creating_dateframe(data=data)
-        poland = Graphs().creating_one_graph(dataframe=df, country_id=(179,), id=True)
+        poland = Graphs().creating_one_graph(dataframe=df, country_id=179)
         return poland
 
 
