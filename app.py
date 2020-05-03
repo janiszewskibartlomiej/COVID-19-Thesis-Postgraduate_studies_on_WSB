a99cb59ef240f7ab722622e9b0003747f3946199
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-05-03 18:54:37 +0200

    update flask - added new end point - graph-diff and logic

--- app.py
+++ app.py
@@ -22,6 +22,15 @@ def graph(id):
     get_graph = Graphs().get_graph(dataframe=df, country_id=id, write=True)
     return render_template('./graphs/' + get_graph[1] + '.html')
 
+@app.route('/graph-diff=<int:id>')
+def graph_diff(id):
+    if id == 0:
+        return render_template('.graphs/world-diff.html')
+
+    data = DataProcessing().all_cases_per_day_where_country_id_equal(country_id=id)
+    df = DataProcessing().get_dateframe_diff(data=data)
+    get_graph = Graphs().get_graph(dataframe=df, country_id=id, write=True, diff=True)
+    return render_template('./graphs/' + get_graph[1] + '.html')
 
 @app.route('/graphs')
 def choice_two_graphs():
@@ -36,5 +45,5 @@ def join_two_graphs(first_id, second_id):
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='192.168.8.108', port=9000, debug=True)
+    app.run(debug=True)
+    # app.run(host='192.168.8.108', port=9000, debug=True)
